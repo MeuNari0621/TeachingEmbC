6b54cd603e53c9f3fdbd50df521a2272a6336aa8
ohnari_loc <o.konajin630@gmail.com>
2026-07-13 00:52:19 +0900

    QAC-B

--- src/dbc/battery_monitor.c
+++ src/dbc/battery_monitor.c
@@ -21,16 +21,22 @@ void battery_monitor_init(battery_monitor_t *ctx,
                           uint16_t critical_mv,
                           uint16_t over_mv)
 {
+    /* QAC-B add */
+    uint8_t null_flg = 0;
+
     /* 防御的処理: NULLポインタ */
     if (ctx == 0) {
-        return;
+        /* QAC-B add */
+        null_flg = 1;
     }
 
-    ctx->low_threshold_mv = low_mv;
-    ctx->critical_threshold_mv = critical_mv;
-    ctx->over_threshold_mv = over_mv;
-    ctx->last_state = BATTERY_STATE_NORMAL;
-    ctx->last_voltage_mv = 0;
+    if( 1 != null_flg){
+        ctx->low_threshold_mv = low_mv;
+        ctx->critical_threshold_mv = critical_mv;
+        ctx->over_threshold_mv = over_mv;
+        ctx->last_state = BATTERY_STATE_NORMAL;
+        ctx->last_voltage_mv = 0;
+    }
 }
 
 uint16_t battery_raw_to_mv(uint16_t raw_adc)
